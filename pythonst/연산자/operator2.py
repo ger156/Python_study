3276e1b6c3c78f3df2dfa1108d686798c5673dc3
DESKTOP-BCFFJET\heemo <ger156>
2023-03-20 22:13:55 +0900

    연산자 및 조건문 공부

---
+++ pythonst/연산자/operator2.py
@@ -0,0 +1,26 @@
+#비교연산자를 해보세요
+
+a = 10
+
+print(a == 10) # 같다.
+
+print(a != 10) # 같지않다.
+
+print(a > 10) # a가 10보다 크다.
+
+print(a >= 10) # a가 10보다 크거나 같다.
+
+b = 10
+
+print(a == b) # a와 b가 같다.
+
+print(a != b) # 같지않다.
+
+c = False #boolean탑입 #c와 False가 같다.
+
+print(c == False) # Ture가 나옵니다.
+
+d = True
+#c와 d가 같다.
+
+print(c == d) # 같이 않으니 False가 나오게 됩니다.
