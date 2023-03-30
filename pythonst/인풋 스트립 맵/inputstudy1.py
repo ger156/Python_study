8361149ba526d818cdf97a4eb24763e0ecf37154
DESKTOP-BCFFJET\heemo <heemoon25@naver.com>
2023-03-30 22:19:37 +0900

    인풋 스트립 맵 공부 연습

---
+++ pythonst/인풋 스트립 맵/inputstudy1.py
@@ -0,0 +1,19 @@
+# 문제 - 사용자에게 문장 1개를 입력받아서, 출력해주세요
+print("문장을 입력 해주세요: ", end = '')
+line = input()
+print(line)
+
+# 문제 - 사용자에게 문장 1개를 입력받아서, ','를 기준으로 나눠주세요
+
+print("문장을 입력 해주세요: ", end = '')
+line = input().split(', ')
+l = line.split(',')
+print(l)
+
+l = []
+l.append(1)
+l.append(2)
+l.append(3)
+
+l = '1,2,3'.split(',')
+print(l)
