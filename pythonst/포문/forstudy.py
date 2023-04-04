cff372f2c4b35b0f2cdc902b8e5e84224f56f4e0
DESKTOP-BCFFJET\heemo <heemoon25@naver.com>
2023-04-04 23:40:52 +0900

    for문 시작

---
+++ pythonst/포문/forstudy.py
@@ -0,0 +1,13 @@
+# print("== while ==")
+# i = 0
+# while i < 10:
+#   print(i)
+#   i += 1
+
+# print("== for ==")
+# for i in range(10, 1, -1):
+#   print(i)
+
+a = [10, 20, 30, 40]
+for i in range(len(a) -1, -1, -1):
+  print(a[i])
